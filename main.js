8f3b0a0519e0e588836a8c09ca4aaf17ce7b0080
Amanda Slayton <alslayton86@gmail.com>
2018-03-02 16:33:09 -0600

    completed Challenge 3

--- main.js
+++ main.js
@@ -1,63 +1,70 @@
-console.log("Hello");
+// console.log("Hello");
 
 // CHALLENGE 1
 
-var challengeOneInput = "jumbo shrimp";
-var SchallengeOneInput = challengeOneInput.split(" ");
-var input1 = SchallengeOneInput[0].split("");
-var input2 = SchallengeOneInput[1].split("");
-var score1;
-var score2;
-var challenge1Element = document.getElementById("challenge-1");
-for(var i = 0; i < input1.length; i++){
-  score1 += input1[i].charCodeAt();
-};
+// var challengeOneInput = "jumbo shrimp";
+// var SchallengeOneInput = challengeOneInput.split(" ");
+// var input1 = SchallengeOneInput[0].split("");
+// var input2 = SchallengeOneInput[1].split("");
+// var score1;
+// var score2;
+// var challenge1Element = document.getElementById("challenge-1");
+// for(var i = 0; i < input1.length; i++){
+//   score1 += input1[i].charCodeAt();
+// };
 
-for(var j = 0; i < input2.length; i++){
-  score2 += input2[i].charCodeAt();
-};
+// for(var j = 0; i < input2.length; i++){
+//   score2 += input2[i].charCodeAt();
+// };
 
-if (score1 > score2){
-  challenge1Element.innerHTML = ("<h1>" + input1.join("") + "</h1>");
-}else {
-  challenge1Element.innerHTML = ("<h1>" + input2.join("") + "</h1>");
-};
+// if (score1 > score2){
+//   challenge1Element.innerHTML = ("<h1>" + input1.join("") + "</h1>");
+// }else {
+//   challenge1Element.innerHTML = ("<h1>" + input2.join("") + "</h1>");
+// };
 
 
 
-// CHALLENGE 2
-var challengeTwoInputA = [1,2,3,4,5,6,3,8,9];
-var challengeTwoInputB = [12,13,14];
-var challengeTwoInputC = [9,2,4,7,3];
-var challenge2Element = document.getElementById("challenge-2");
-for(var i = 0; i < challengeTwoInputB.length; i++){
-  var changeMe = challengeTwoInputB[i].toString();
-  if (changeMe.includes('3')){
-    challengeTwoInputB[i] = changeMe.replace(/[3]/g, 7) * 1;
-  }else if (changeMe.includes('7')){
-    challengeTwoInputB[i] = changeMe.replace(/[7]/g, 3) * 1;
-  }else {
-    challengeTwoInputB[i]=changeMe * 1;
-  };
-};
-challenge2Element.innerHTML = ("<h1>" + challengeTwoInputB + "</h1>");
+// // CHALLENGE 2
+// var challengeTwoInputA = [1,2,3,4,5,6,3,8,9];
+// var challengeTwoInputB = [12,13,14];
+// var challengeTwoInputC = [9,2,4,7,3];
+// var challenge2Element = document.getElementById("challenge-2");
+// for(var i = 0; i < challengeTwoInputB.length; i++){
+//   var changeMe = challengeTwoInputB[i].toString();
+//   if (changeMe.includes('3')){
+//     challengeTwoInputB[i] = changeMe.replace(/[3]/g, 7) * 1;
+//   }else if (changeMe.includes('7')){
+//     challengeTwoInputB[i] = changeMe.replace(/[7]/g, 3) * 1;
+//   }else {
+//     challengeTwoInputB[i]=changeMe * 1;
+//   };
+// };
+// challenge2Element.innerHTML = ("<h1>" + challengeTwoInputB + "</h1>");
 
 
 
 // CHALLENGE 3
-// var challengeThreeInputA = [ 1, 1, 1, 2, 1, 1 ]; 
-// var challengeThreeInputB = [ 0, 0, 0, 0, 0, 0, 0, 0.55, 0, 0 ];
-// var maxInput = Math.max.apply(Math,challengeThreeInputA);
-// console.log(maxInput);
-// var minInput = Math.min.apply(Math,challengeThreeInputA);
-// console.log(minInput);
+var theArray = [1,1,1,1,2,1,1,1];
+var maxArray = Math.max.apply(Math, theArray);
 
-// for(var i = 0; i < challengeThreeInputA; i++){
-//   if (maxInput === challengeThreeInputA[i]){
-//     var exists = 1;
-//   };
-// };
+var minArray = Math.min.apply(Math, theArray);
 
+var countMeMax = 0;
+var countMeMin = 0;
+for(var i = 0; i < theArray.length; i++){
+  if(maxArray === theArray[i]){
+    countMeMax += 1;
+  }else if (minArray === theArray[i]){
+    countMeMin += 1;
+  };
+};
+
+if(countMeMax > countMeMin){
+  document.getElementById("challenge-3").innerHTML = minArray;
+}else{
+  document.getElementById("challenge-3").innerHTML = maxArray;
+};
 
 
 // CHALLENGE 4
