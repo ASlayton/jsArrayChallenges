900da384dcacdbc214d280d794f7547416c51967
Amanda Slayton <alslayton86@gmail.com>
2018-03-01 22:42:57 -0600

    Challenge 2 complete

--- main.js
+++ main.js
@@ -19,43 +19,44 @@ for(var j = 0; i < input2.length; i++){
 
 if (score1 > score2){
   challenge1Element.innerHTML = ("<h1>" + input1.join("") + "</h1>");
-  console.log(input1.join(""));
 }else {
   challenge1Element.innerHTML = ("<h1>" + input2.join("") + "</h1>");
-  console.log(input2.join(""));
 };
+
+
 
 // CHALLENGE 2
-// var challengeTwoInputA = [1,2,3,4,5,6,3,8,9];
-// var challengeTwoInputB = [12,13,14];
-// var challengeTwoInputC = [9,2,4,7,3];
-
-// for(var i = 0; i < challengeTwoInputB.length; i++){
-//   var changeMe = challengeTwoInputB[i].toString();
- 
-//   if (changeMe.includes('3')){
-//     challengeTwoInputB[i] = changeMe.replace(/[3]/g, 7) * 1;
-//   }else if (changeMe.includes('7')){
-//     challengeTwoInputB[i] = changeMe.replace(/[7]/g, 3) * 1;
-//   }else {
-//     challengeTwoInputB[i]=changeMe * 1;
-//   };
-// };
-// console.log(challengeTwoInputB);
-
-// CHALLENGE 3
-var challengeThreeInputA = [ 1, 1, 1, 2, 1, 1 ]; 
-var challengeThreeInputB = [ 0, 0, 0, 0, 0, 0, 0, 0.55, 0, 0 ];
-var maxInput = Math.max.apply(Math,challengeThreeInputA);
-console.log(maxInput);
-var minInput = Math.min.apply(Math,challengeThreeInputA);
-console.log(minInput);
-
-for(var i = 0; i < challengeThreeInputA; i++){
-  if (maxInput === challengeThreeInputA[i]){
-    var exists = 1;
+var challengeTwoInputA = [1,2,3,4,5,6,3,8,9];
+var challengeTwoInputB = [12,13,14];
+var challengeTwoInputC = [9,2,4,7,3];
+var challenge2Element = document.getElementById("challenge-2");
+for(var i = 0; i < challengeTwoInputB.length; i++){
+  var changeMe = challengeTwoInputB[i].toString();
+  if (changeMe.includes('3')){
+    challengeTwoInputB[i] = changeMe.replace(/[3]/g, 7) * 1;
+  }else if (changeMe.includes('7')){
+    challengeTwoInputB[i] = changeMe.replace(/[7]/g, 3) * 1;
+  }else {
+    challengeTwoInputB[i]=changeMe * 1;
   };
 };
+challenge2Element.innerHTML = ("<h1>" + challengeTwoInputB + "</h1>");
+
+
+
+// CHALLENGE 3
+// var challengeThreeInputA = [ 1, 1, 1, 2, 1, 1 ]; 
+// var challengeThreeInputB = [ 0, 0, 0, 0, 0, 0, 0, 0.55, 0, 0 ];
+// var maxInput = Math.max.apply(Math,challengeThreeInputA);
+// console.log(maxInput);
+// var minInput = Math.min.apply(Math,challengeThreeInputA);
+// console.log(minInput);
+
+// for(var i = 0; i < challengeThreeInputA; i++){
+//   if (maxInput === challengeThreeInputA[i]){
+//     var exists = 1;
+//   };
+// };
 
 
 
